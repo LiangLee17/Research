af9a154b6ee18d864c1d28a6cd3bfa1a91ad68ce
lianglee17 <lianglee17@qq.com>
2022-04-21 11:33:52 +0800

    add AFEPack

---
+++ AFEPack/config.h
@@ -0,0 +1,35 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `dl' library (-ldl). */
+#define HAVE_LIBDL 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Name of package */
+#define PACKAGE "afepack"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "rli@math.pku.edu.cn"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "AFEPack"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "AFEPack 1.8"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "afepack"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.8"
+
+/* Define to 1 if you have the ANSI C header files. */
+/* #undef STDC_HEADERS */
+
+/* Version number of package */
+#define VERSION "1.8"
